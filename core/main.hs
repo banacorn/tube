3ae5a2c7e63201e85d7d6f75cdf5d483a1575d0c
Ting-Yen LAI <banacorn@gmail.com>
2013-05-03 20:39:33 +0800

    linear time dispatching

--- core/main.hs
+++ core/main.hs
@@ -13,6 +13,10 @@ import Data.ByteString (ByteString)
 import Data.Array.IO
 
 
+--
+--  some redis shit
+--
+
 connectAndRun f = connect defaultConnectInfo >>= flip runRedis f
 
 getTubesSettings :: RedisCtx m f => m (f [(ByteString, ByteString)])
@@ -25,43 +29,19 @@ ack = pubSub (subscribe ["tubes"]) $ \msg -> do
       putStrLn $ "Message from " ++ show (msgMessage msg)
       return mempty
 
---data Type = LowRes | HighRes | LowCom | HighCom deriving (Eq)
---type Address = (Int, Int)
---type Population = Int
---type Destination = (Address, Population)
---type Flow = (Int, Int)
---data Block = Block Type Flow [Destination]
+--
+--  
+--
 
---instance Show Type where
---    show LowRes = "r"
---    show HighRes = "R"
---    show LowCom = "c"
---    show HighCom = "C"
+data Population = In Int | Out Int deriving (Show, Eq)
+data Block  = Block Population Population deriving (Show, Eq)
 
---instance Show Block where
---    show (Block blockType (i, o) destinations) = "[ " ++ show blockType ++ " " ++ show i ++ "+ " ++ show o ++ "- ]"
-
---typeFlow :: Type -> Flow
---typeFlow LowRes = (0, 200)
---typeFlow HighRes = (100, 800)
---typeFlow LowCom = (200, 0)
---typeFlow HighCom = (800, 100)
-
---a = Block LowCom (typeFlow LowCom) [] --[((2, 4) 100)]
-
---data City = City Int [Block]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+--  linear time dispatching (deterministic, not ideal)
+type Probability = Float
+dispatch :: [Probability] -> Int -> [Int]
+dispatch probs quantity = sprinkle baseShare (quantity - baseSum)
+    where   size = length probs
+            baseShare = map (\p -> floor $ fromIntegral quantity * p) probs
+            baseSum = sum baseShare
+            sprinkle xs 0 = xs
+            sprinkle (x:xs) n = (succ x):sprinkle xs (pred n)
