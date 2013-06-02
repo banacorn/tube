8f362533d00be7f7a7877a3aec2ab2c2b306293a
Ting-Yen LAI <banacorn@gmail.com>
2013-06-02 22:05:42 +0800

    /simulation/:id map rendering

--- client/scripts/view/simulation.js
+++ client/scripts/view/simulation.js
@@ -9,24 +9,33 @@ define([
 
    
     var SimulationView = Backbone.View.extend({
+        mapSize: 600,
         template: Hogan.compile($$simulation),
 
         initialize: function () {
             var self = this;
+            
             this.model = new Map({
                 id: this.id
             });
-            this.model.on('change', function (model) {
-                console.log(model)
-                self.mapView = new MapView({
-                    model: model,
-                    attributes: {
-                        width: 500,
-                        height: 500
-                    }
-                });
-                self.render();
+
+            this.mapView = new MapView({
+                model: this.model,
+                attributes: {
+                    width: this.mapSize,
+                    height: this.mapSize
+                }
             });
+
+
+            this.model.on('change', function (model) {
+                self.render();
+                self.renderMap();
+
+                console.log('renewed')
+            });
+            this.render();
+            this.renderMap();
             this.model.fetch();
             // console.log(this.simulationModel);
             // this.simulationModel.on('change', function (model) {
@@ -64,22 +73,12 @@ define([
 
         render: function () {
             this.$el.html(this.template.render(this.model.toJSON()));
-            // this.renderMap();
             return this;
+        },
+        renderMap: function () {
+            console.log('renderMap!!');
+            $('.simulation-map', this.$el).append(this.mapView.el);
         }
-        // renderMap: function () {
-
-        //     var viewSize = 280;
-        //     var viewMargin = 0;
-        //     this.mapView = new MapView({
-        //         model: this.model,
-        //         attributes: {
-        //             width: viewSize - viewMargin * 2,
-        //             height: viewSize - viewMargin * 2
-        //         }
-        //     });
-        //     $('figcaption', this.$el).after(this.mapView.el);
-        // }
 
     });
 
